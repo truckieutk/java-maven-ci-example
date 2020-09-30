a77f89103fd61c62b90b19939e060b55a1011b4d
Pritesh Patel <patelpritesh2484@gmail.com>
2020-09-30 11:43:18 -0400

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# How To Start
+
+1. [Fork](https://github.com/pritamworld/java-maven-travis-ci-example/fork) this Repository
+2. Go to [Travis CI](http://travis-ci.com) and enable the repository
---
+++ src/main/java/com/pritesh/SimpleCalculator.java
@@ -0,0 +1,23 @@
+package com.pritesh;
+
+public class SimpleCalculator {
+  public static void main(String[] args) {
+    System.out.println("Hello world from SimpleCalculator");
+  }
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int sub(int a, int b) {
+    return a - b;
+  }
+
+  public int mul(int a, int b) {
+    return a * b;
+  }
+
+  public int div(int a, int b) {
+    return a / b;
+  }
+}
